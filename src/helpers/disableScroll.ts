417f0acc317dfd3f73ca8689f39fa01d1d9e269d
Aleksey Makhankov <aleksey.makhankov@gmail.com>
2020-02-20 22:08:39 +0300

    fix disable scroll

--- src/helpers/disableScroll.ts
+++ src/helpers/disableScroll.ts
@@ -1,9 +1,11 @@
+import styles from '../style.scss'
+
 export const disableScroll = (disable = false) => {
   if (disable) {
-    document.documentElement.classList.add('body-noscroll')
-    document.body.classList.add('body-noscroll')
+    document.documentElement.classList.add(styles.bodyNoscroll)
+    document.body.classList.add(styles.bodyNoscroll)
   } else {
-    document.documentElement.classList.remove('body-noscroll')
-    document.body.classList.remove('body-noscroll')
+    document.documentElement.classList.remove(styles.bodyNoscroll)
+    document.body.classList.remove(styles.bodyNoscroll)
   }
 }
